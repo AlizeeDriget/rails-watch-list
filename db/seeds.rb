6c3a879756f7998d0ae19ca32a9cad0bc5716d0b
Alizee Driget <driget.alizee@gmail.com>
2025-05-29 18:15:07 +0800

    seed with API

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "json"
+require "open-uri"
+
+omdbapiUrl = "http://www.omdbapi.com/?s=french&type=movie&apikey=adf1f2d7";
+
+movie_serialized = URI.parse(omdbapiUrl).read
+movies_hash = JSON.parse(movie_serialized)
+movies = movies_hash["Search"]
+
 puts "Cleaning database..."
+Bookmark.destroy_all
 Movie.destroy_all
 
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-
-puts "Finished! Created #{Movie.count} restaurants."
+movies.each do |movie|
+  Movie.create!(title: movie["Title"], overview: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit#{movie["Title"]}", poster_url: movie["Poster"], rating: 6.9)
+end
+puts "Finished! Created #{Movie.count} movies."
